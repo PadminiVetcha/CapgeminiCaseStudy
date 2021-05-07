3c8e7eb357482dec055b24d3b7479be7e9b6a2e2
PadminiVetcha <53965931+PadminiVetcha@users.noreply.github.com>
2021-05-07 16:15:56 +0530

    Delete CancelATicketService directory

--- CancelATicketService/src/main/java/org/padmini/railway/dao/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package org.padmini.railway.dao;
-import org.padmini.railway.entity.UserDetails;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-//Dao Layer
-public interface UserRepository extends MongoRepository<UserDetails, Integer>
-{
-}
