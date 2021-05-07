96404af500064c775c1aa7775a817416c2e4904f
vetch <vetchapaddu13@gmail.com>
2021-05-07 08:40:54 +0530

    Made changes

--- BookATicketService/src/main/java/org/padmini/railway/controller/BookUserController.java
+++ BookATicketService/src/main/java/org/padmini/railway/controller/BookUserController.java
@@ -1,10 +1,13 @@
 package org.padmini.railway.controller;
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.padmini.railway.entity.PaymentDetails;
 import org.padmini.railway.entity.UserDetails;
 import org.padmini.railway.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,12 +36,20 @@ public class BookUserController
 		userDetails.setPayment("Pending");
 		userServiceImpl.addUserDetails(userDetails);
 		return ("Your ticket id booked successfully...!!!  "
-				+ "Your pnr number is "+ userDetails.getPnrNo());
+				+ "Your pnr number is "+ userDetails.getPnrNo() + " Please proceed to payment....");
 	}
-	/*
-	 * @GetMapping("/all") public List<TrainDetails> getAllTrainDetails() { return
-	 * userServiceImpl.getAllTrainDetails(); }
-	 */
+	
+	@GetMapping("/all")
+	public List<UserDetails> getAll()
+	{
+		return userServiceImpl.getAll();
+	}
+	
+	@DeleteMapping("/{pnrNo}")
+	public String deleteUserDetailsById(@PathVariable long pnrNo)
+	{
+		return userServiceImpl.deleteBookingDetails(pnrNo);
+	}
 	
 	
 	  @PostMapping("/pay/") 
--- BookATicketService/src/main/java/org/padmini/railway/service/UserService.java
+++ BookATicketService/src/main/java/org/padmini/railway/service/UserService.java
@@ -1,11 +1,15 @@
 package org.padmini.railway.service;
+import java.util.List;
+
 import org.padmini.railway.entity.PaymentDetails;
 import org.padmini.railway.entity.UserDetails;
 
 public interface UserService 
 {
+	public List<UserDetails> getAll();
 	public UserDetails getUserDetailsById(long pnrNo);
 	public void addUserDetails(UserDetails userDetails);
 	public void proceedToPay(PaymentDetails payment);
+	public String deleteBookingDetails(long pnrNo); 
 	
 }
